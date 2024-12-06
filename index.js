0622826ce08df4edad96d226a6e88ef67a660439
Happiness Maina <mainahappiness361@gmail.com>
2024-12-06 10:01:48 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+    const messages = [];
+    
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    
+    return messages;
+}
+function countDown(startingNumber) {
+    for (let i = startingNumber; i >= 0; i--) {
+        console.log(i); // Log the current number
+    }
+}
